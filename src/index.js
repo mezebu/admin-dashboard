f65e913a23ede2160ddb59ad70248aa6774a7823
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-04-28 19:02:55 +0100

    Updates

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { theme } from "./styles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );

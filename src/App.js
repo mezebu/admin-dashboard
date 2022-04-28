c6db5007e4084736a7901a5b981f718603ec082b
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-04-28 19:02:25 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Home, List, Login, New, Single } from "./Pages";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="users">
+            <Route index element={<List />} />
+            <Route path=":userId" element={<New />} />
+            <Route path="single" element={<Single />} />
+          </Route>
+          <Route path="products">
+            <Route index element={<List />} />
+            <Route path=":productId" element={<New />} />
+            <Route path="product" element={<Single />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

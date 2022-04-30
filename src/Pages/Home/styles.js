b40e92095506cfaed0efd54cf7ca77d4a2fc9a78
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-04-30 13:25:20 +0100

    Updated styles

--- src/Pages/Home/styles.js
+++ src/Pages/Home/styles.js
@@ -2,11 +2,8 @@ import { Box, styled } from "@mui/material";
 
 const Main = styled(Box)({
   display: "flex",
-  padding: "15px 15px 0 15px",
 });
 
-const MainContainer = styled(Box)({
-  flex: 5,
-});
+const MainContainer = styled(Box)({});
 
 export { Main, MainContainer };

f1ee24ba62e75ef198dd9bc3cd70ebca6e843a84
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-02 14:38:58 +0100

    Update data.js

--- src/components/TableList/data.js
+++ src/components/TableList/data.js
@@ -9,7 +9,7 @@ const rows = [
     amount: 500,
     company: "Deckow-Crist",
     method: "Cash on delivery",
-    approved: true,
+    status: "Approved",
   },
   {
     id: 7293647930,
@@ -21,19 +21,7 @@ const rows = [
     amount: 499,
     company: "Romaguera-Jacobson",
     method: "Online Payment",
-    approved: true,
-  },
-  {
-    id: 3486108352,
-    customer: "Clementine Bauch",
-    username: "Samantha",
-    image:
-      "https://images.unsplash.com/photo-1525134479668-1bee5c7c6845?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-    product: "Playstation 4",
-    amount: 499,
-    company: "Romaguera-Jacobson",
-    method: "Online Payment",
-    approved: true,
+    status: "Approved",
   },
   {
     id: 4082763990,
@@ -45,8 +33,21 @@ const rows = [
     amount: 499,
     company: "Romaguera-Jacobson",
     method: "Online Payment",
-    approved: false,
+    status: "Pending",
   },
+  {
+    id: 3486108352,
+    customer: "Clementine Bauch",
+    username: "Samantha",
+    image:
+      "https://images.unsplash.com/photo-1525134479668-1bee5c7c6845?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    product: "Playstation 4",
+    amount: 499,
+    company: "Romaguera-Jacobson",
+    method: "Online Payment",
+    status: "Approved",
+  },
+
   {
     id: 8398264422,
     customer: "Chelsey Dietrich",
@@ -57,7 +58,7 @@ const rows = [
     amount: 499,
     company: "Romaguera-Jacobson",
     method: "Online Payment",
-    approved: false,
+    status: "Pending",
   },
   {
     id: 9036782637,
@@ -69,7 +70,7 @@ const rows = [
     amount: 499,
     company: "Romaguera-Jacobson",
     method: "Cash on delivery",
-    approved: true,
+    status: "Approved",
   },
 ];
 

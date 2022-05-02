c376017599f13bb0d4b4a51b4f9e9df304590b70
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-02 14:39:33 +0100

    Update Login.jsx

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,14 +1,78 @@
 import React from "react";
-import { Box, Toolbar, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const Login = () => {
   return (
-    <>
-      <Box component="main">
-        <Toolbar />
-        <Typography>Login</Typography>
-      </Box>
-    </>
+    <Box component="div">
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+        dolorum doloribus mollitia sint repellat ipsam tempora itaque quis
+        distinctio, dolorem nostrum, rem totam maiores? Commodi pariatur sequi
+        ex ducimus quas! Magnam pariatur illo consectetur tempore, nulla
+        incidunt voluptatem expedita a eum laborum quas maxime debitis delectus
+        minima tempora temporibus vel! Sequi alias fugiat quo et, fuga deserunt
+        repellendus quod nesciunt? Aliquid in velit minus, magnam quasi
+        officiis, commodi cumque animi tenetur sit odio facere. Ab deleniti
+        possimus ex, beatae, id quia perferendis totam facere consectetur
+        voluptate labore, ut porro ducimus. Eveniet optio rerum minus, error
+        perspiciatis, aliquam similique deleniti praesentium esse corrupti
+        doloremque suscipit quidem sequi. Harum quam illo modi, iure inventore,
+        id, cum nisi vero nemo ipsam quod magnam? Iste dolore quod corporis
+        eligendi. Tempora velit optio reprehenderit id praesentium architecto
+        obcaecati eos quas? Laudantium, est excepturi iste iusto similique
+        sapiente iure aliquam error fuga nihil at corrupti suscipit? Obcaecati
+        modi deserunt ipsum voluptas in voluptatibus vitae cum ut doloremque
+        voluptatum dolores sit sint eaque placeat laudantium iste, labore
+        temporibus aperiam? Ad dolore nisi beatae voluptatem tenetur aspernatur
+        quo? Magnam architecto, debitis odio officiis placeat nobis eligendi
+        voluptates a aspernatur natus ut molestiae dolor recusandae, fugit
+        molestias corrupti, minima totam voluptate dolorem quidem eos sunt
+        veniam nostrum mollitia! Harum. Doloremque sit sequi nesciunt similique
+        distinctio praesentium, reprehenderit quidem? Laudantium aliquid
+        cupiditate perspiciatis odio vel asperiores, placeat dignissimos, quod
+        impedit voluptate eaque voluptatibus fugiat! Tempora minus odit possimus
+        hic repellendus? Non minima velit tempora molestias repellendus quas
+        esse quibusdam quasi, fuga praesentium magni itaque omnis enim dolor
+        aliquam, perferendis numquam dolorem pariatur animi recusandae delectus
+        vero sequi iste perspiciatis! Explicabo? Vel a minima, maiores aperiam
+        facilis minus eligendi nesciunt dicta sequi vitae dolor distinctio
+        dolorum tenetur asperiores omnis. Officiis iste eius natus culpa quae.
+        Beatae omnis fuga dolorem sit delectus. Quia quam ad obcaecati minus
+        laudantium eveniet nisi veniam, odio velit cum. Possimus, vero, facilis
+        aut cupiditate repellat, velit asperiores tenetur voluptates eaque
+        laboriosam pariatur odio dolore voluptas ipsum cumque! Vero aut pariatur
+        rerum excepturi ducimus tempora necessitatibus reprehenderit corporis.
+        Labore quisquam quae vero, harum rem porro ab corrupti pariatur iure
+        quas, consectetur delectus, voluptas cupiditate? Molestias atque harum
+        architecto! Sit magni, ut at, quo voluptates sint, quod dolor debitis
+        eum quas recusandae pariatur deleniti cumque perspiciatis porro a. Porro
+        eaque quam distinctio cumque aperiam beatae possimus voluptate ex iste!
+        Dolor optio voluptatibus quam dolorem ullam cumque corrupti ipsa
+        inventore vitae dolores iste obcaecati et, quibusdam, nisi voluptas sunt
+        hic, in id consectetur? Explicabo saepe quis nulla tempora, enim
+        voluptatibus? Debitis soluta fugiat aut. Nostrum optio praesentium ipsum
+        soluta sequi, laboriosam dolorem minima quasi odit labore saepe, iure
+        inventore nisi. Ipsa natus voluptatem modi quidem velit explicabo
+        molestiae illum veritatis. Consequuntur totam, harum earum eveniet
+        expedita nobis dolores voluptatem recusandae illum? Autem voluptatibus
+        provident consequatur id facere quam maiores vero? Provident nisi
+        praesentium, officia neque dolor labore cum in beatae? Necessitatibus,
+        magni. Sit, adipisci voluptates temporibus maiores, obcaecati cumque
+        repellat facilis aut perferendis consectetur maxime ducimus recusandae
+        impedit modi aliquid officia dolore magni molestias, fugiat explicabo
+        vitae corporis iure doloribus! Maxime accusamus, saepe assumenda
+        exercitationem at pariatur tempore officia laboriosam vel eveniet quod
+        vitae ipsam quaerat consequatur eius aliquam. Eum non tempora placeat
+        ullam, voluptate error debitis libero rem nulla? Molestiae ipsam
+        voluptates nisi dolore? Fugit eaque enim vero reiciendis assumenda.
+        Magnam accusamus animi nobis veniam itaque molestias rem neque eius
+        atque amet, cumque ad pariatur? Neque voluptatum quae quam. Aliquid odit
+        blanditiis recusandae accusantium neque nulla sit aliquam sed adipisci
+        quam maxime excepturi, assumenda, nemo veniam consequatur iusto. Facere
+        blanditiis voluptatum exercitationem magnam perferendis necessitatibus
+        porro non molestias eveniet!
+      </Typography>
+    </Box>
   );
 };
 

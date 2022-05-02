3915442889e11eed7b686e5b270d62b50a848919
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-02 14:37:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import { Home, List, Login, New, Single } from "./Pages";
+import PageRoutes from "./PageRoutes/PageRoutes";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="users">
-            <Route index element={<List />} />
-            <Route path=":userId" element={<New />} />
-            <Route path="single" element={<Single />} />
-          </Route>
-          <Route path="products">
-            <Route index element={<List />} />
-            <Route path=":productId" element={<New />} />
-            <Route path="product" element={<Single />} />
-          </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <PageRoutes />
+    </div>
   );
 }
 

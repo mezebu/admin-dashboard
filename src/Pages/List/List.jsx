e725c53edd1c5ca0e5f6c131549ea35bd53b59d1
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-02 14:39:30 +0100

    Update List.jsx

--- src/Pages/List/List.jsx
+++ src/Pages/List/List.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Box } from "@mui/material";
+
+import DataTable from "../../components/DataTable/DataTable";
 
 const List = () => {
-  return <div>List</div>;
+  return (
+    <Box component="div">
+      <DataTable />
+    </Box>
+  );
 };
 
 export default List;

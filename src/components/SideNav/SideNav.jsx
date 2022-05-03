3cfdacfd45c03a00bddd5bceeb3f44790fd5b5f1
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-03 12:25:00 +0100

    Create SideNav.jsx

---
+++ src/components/SideNav/SideNav.jsx
@@ -0,0 +1,47 @@
+import { Box, Drawer } from "@mui/material";
+import React from "react";
+import { drawer } from "./SidebarItems";
+
+const SideNav = ({ drawerWidth, mobileOpen, drawerToggle }) => {
+  return (
+    <Box
+      component="nav"
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+      aria-label="mailbox folders"
+    >
+      {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+      <Drawer
+        variant="temporary"
+        open={mobileOpen}
+        onClose={drawerToggle}
+        ModalProps={{
+          keepMounted: true,
+        }}
+        sx={{
+          display: { xs: "block", sm: "none" },
+          "& .MuiDrawer-paper": {
+            boxSizing: "border-box",
+            width: drawerWidth,
+          },
+        }}
+      >
+        {drawer}
+      </Drawer>
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: { xs: "none", sm: "block" },
+          "& .MuiDrawer-paper": {
+            boxSizing: "border-box",
+            width: drawerWidth,
+          },
+        }}
+        open
+      >
+        {drawer}
+      </Drawer>
+    </Box>
+  );
+};
+
+export default SideNav;

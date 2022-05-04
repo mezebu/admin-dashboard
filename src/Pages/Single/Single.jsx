749d0d9284f2bc560e8e08e4e45c078fa4c5570a
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-04 09:53:09 +0100

    Update Single.jsx

--- src/Pages/Single/Single.jsx
+++ src/Pages/Single/Single.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Box, Divider, Grid, Typography } from "@mui/material";
+import ProfileDetails from "./ProfileDetails/ProfileDetails";
+import ProfileWidgets from "./ProfileWidgets/ProfileWidgets";
+import Skills from "./Skills/Skills";
 
-const User = () => {
-  return <div>Single</div>;
+const Single = () => {
+  return (
+    <Box component="div" sx={{ width: "100%" }}>
+      <Box sx={{ marginBottom: 5 }}>
+        <Typography
+          variant="h5"
+          sx={{ fontWeight: 600, marginBottom: 3 }}
+          gutterBottom
+        >
+          Profile
+        </Typography>
+        <Divider />
+      </Box>
+
+      <Grid container spacing={2}>
+        <Grid item lg={4} md={12} sm={12} xs={12}>
+          <ProfileDetails />
+          <Skills />
+        </Grid>
+        <Grid item lg={8} md={12} sm={12} xs={12}>
+          <ProfileWidgets />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
-export default User;
+export default Single;

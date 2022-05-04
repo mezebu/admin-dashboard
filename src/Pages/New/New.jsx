853d29b30940a5d785b4f871200ad7c85166382b
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-04 09:52:36 +0100

    Update New.jsx

--- src/Pages/New/New.jsx
+++ src/Pages/New/New.jsx
@@ -1,10 +1,30 @@
 import React from "react";
-import { Box, Divider, Grid, Typography } from "@mui/material";
-import ProfileDetails from "./ProfileDetails/ProfileDetails";
-import ProfileWidgets from "./ProfileWidgets/ProfileWidgets";
-import Skills from "./Skills/Skills";
+import {
+  Avatar,
+  Badge,
+  Box,
+  Divider,
+  Grid,
+  IconButton,
+  Paper,
+  styled,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
 
-const NewUser = () => {
+const Input = styled("input")({
+  display: "none",
+});
+
+const SmallAvatar = styled(Avatar)(({ theme }) => ({
+  width: 22,
+  height: 22,
+  border: `2px solid ${theme.palette.background.paper}`,
+}));
+
+const New = ({ inputs, title }) => {
   return (
     <Box component="div" sx={{ width: "100%" }}>
       <Box sx={{ marginBottom: 5 }}>
@@ -13,22 +33,68 @@ const NewUser = () => {
           sx={{ fontWeight: 600, marginBottom: 3 }}
           gutterBottom
         >
-          Profile
+          {title}
         </Typography>
         <Divider />
-      </Box>
 
-      <Grid container spacing={2}>
-        <Grid item lg={4} md={12} sm={12} xs={12}>
-          <ProfileDetails />
-          <Skills />
-        </Grid>
-        <Grid item lg={8} md={12} sm={12} xs={12}>
-          <ProfileWidgets />
-        </Grid>
-      </Grid>
+        <Toolbar />
+        <Paper elevation={0.8}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={12} md={4}>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  height: 300,
+                }}
+              >
+                <label htmlFor="icon-button-file">
+                  <Input accept="image/*" id="icon-button-file" type="file" />
+                  <IconButton
+                    color="primary"
+                    aria-label="upload picture"
+                    component="span"
+                  >
+                    <Badge
+                      overlap="circular"
+                      anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                      badgeContent={<PhotoCamera />}
+                    >
+                      <SmallAvatar
+                        alt="profile"
+                        sx={{ width: 100, height: 100 }}
+                      />
+                    </Badge>
+                  </IconButton>
+                </label>
+              </Box>
+            </Grid>
+
+            <Grid item xs={12} sm={12} md={8}>
+              <Box sx={{ p: 2 }}>
+                <Grid container spacing={2}>
+                  {inputs.map(({ id, type, label, placeholder }) => (
+                    <Grid item xs={12} sm={12} md={12} lg={6} key={id}>
+                      <TextField
+                        fullWidth
+                        id={id}
+                        label={label}
+                        type={type}
+                        autoFocus
+                        sx={{ mt: 2 }}
+                        variant="standard"
+                      />
+                    </Grid>
+                  ))}
+                </Grid>
+              </Box>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Box>
     </Box>
   );
 };
 
-export default NewUser;
+export default New;

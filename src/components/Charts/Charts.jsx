1a10b0d7a76eecbca3bbbaf078f70550462ce6c5
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-05 13:50:14 +0100

    Update Charts.jsx

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Grid } from "@mui/material";
 import Chart from "./Chart/Chart";
 import Features from "./Features/Features";
+import TableList from "../TableList/TableList";
 
 const Charts = () => {
   return (
@@ -11,6 +12,9 @@ const Charts = () => {
       </Grid>
       <Grid item lg={4} md={12} sm={12} xs={12}>
         <Features />
+      </Grid>
+      <Grid item lg={12} md={12} sm={12} xs={12}>
+        <TableList />
       </Grid>
     </Grid>
   );

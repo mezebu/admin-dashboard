3a1419ee5d76b5062472477e1edeed5c53fba9a5
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-07 09:52:36 +0100

    Create AuthContext.js

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,24 @@
+import { createContext, useEffect, useReducer } from "react";
+import AuthReducer from "./AuthReducer";
+
+const INITIAL_STATE = {
+  currentUser: JSON.parse(localStorage.getItem("user")) || null,
+};
+
+const AuthContext = createContext(INITIAL_STATE);
+
+const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.currentUser));
+  }, [state.currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser: state.currentUser, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthContextProvider };

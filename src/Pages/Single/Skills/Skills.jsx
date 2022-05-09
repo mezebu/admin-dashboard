74271d655c43b2c8797b2f286779db8b327e8543
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-09 07:35:24 +0100

    Update Skills.jsx

--- src/Pages/Single/Skills/Skills.jsx
+++ src/Pages/Single/Skills/Skills.jsx
@@ -1,5 +1,15 @@
-import { Box, Card, CardContent, Typography } from "@mui/material";
+import { Box, Card, CardContent, styled, Typography } from "@mui/material";
 import React from "react";
+
+const StyledTypography = styled(Typography)(({ theme }) => ({
+  background: theme.palette.info.main,
+  paddingTop: 0.1,
+  paddingBottom: 0.1,
+  paddingLeft: 4,
+  paddingRight: 4,
+  borderRadius: theme.shape.borderRadius,
+  color: theme.palette.success,
+}));
 
 const Skills = () => {
   return (
@@ -7,6 +17,13 @@ const Skills = () => {
       <Card elevation={0}>
         <CardContent>
           <Typography sx={{ fontWeight: 600, pb: 3 }}>Skills</Typography>
+          <Box sx={{ display: "flex", justifyContent: "space-around" }}>
+            <StyledTypography>React</StyledTypography>
+            <StyledTypography>Javascript</StyledTypography>
+            <StyledTypography>Redux</StyledTypography>
+            <StyledTypography>Next.js</StyledTypography>
+            <StyledTypography>Saas</StyledTypography>
+          </Box>
         </CardContent>
       </Card>
     </Box>

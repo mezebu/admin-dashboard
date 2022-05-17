e9362cdb9ec2f69276b258a2d6550d270f5d154d
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-17 10:52:27 +0100

    Update index.js

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -3,3 +3,4 @@ export { default as Login } from "./Login/Login";
 export { default as List } from "./List/List";
 export { default as Single } from "./Single/Single";
 export { default as New } from "./New/New";
+export { default as Analytics } from "./Analytics/Analytics";

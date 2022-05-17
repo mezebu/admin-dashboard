1238d96730f7577884019178ae4769e23d3bcc1d
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-17 10:51:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { useContext } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 
-import { Home, List, Login, New, Single } from "./Pages";
+import { Home, List, Login, New, Single, Analytics } from "./Pages";
 import { formInputs, products } from "./formInputs";
 import { useThemeContext } from "./contexts/ThemeContext";
 import { darkMode, lightMode } from "./styles";
 import Product from "./Pages/Product/Product";
 import { AuthContext } from "./contexts/AuthContext";
+
 
 function App() {
   const darkTheme = useThemeContext();
@@ -24,66 +25,17 @@ function App() {
         <Routes>
           <Route path="/">
             <Route path="login" element={<Login />} />
-            <Route
-              index
-              element={
-                <RequireAuth>
-                  <Home />
-                </RequireAuth>
-              }
-            />
+            <Route index element={<RequireAuth><Home /></RequireAuth>} />        
             <Route path="users">
-              <Route
-                index
-                element={
-                  <RequireAuth>
-                    <List />
-                  </RequireAuth>
-                }
-              />
-              <Route
-                path=":userId"
-                element={
-                  <RequireAuth>
-                    {" "}
-                    <Single />
-                  </RequireAuth>
-                }
-              />
-              <Route
-                path="new"
-                element={
-                  <RequireAuth>
-                    <New inputs={formInputs} title="Add new user" />
-                  </RequireAuth>
-                }
-              />
+              <Route index element={<RequireAuth><List /></RequireAuth>}/>          
+              <Route path=":userId" element={<RequireAuth><Single /></RequireAuth>}/>     
+              <Route path="new" element={<RequireAuth><New inputs={formInputs} title="Add new user" /></RequireAuth>}/>               
+              <Route path="analytics" element={<RequireAuth><Analytics /></RequireAuth>}/>               
             </Route>
             <Route path="products">
-              <Route
-                index
-                element={
-                  <RequireAuth>
-                    <List />
-                  </RequireAuth>
-                }
-              />
-              <Route
-                path=":productId"
-                element={
-                  <RequireAuth>
-                    <Product />
-                  </RequireAuth>
-                }
-              />
-              <Route
-                path="new"
-                element={
-                  <RequireAuth>
-                    <New inputs={products} title="Add new Product" />
-                  </RequireAuth>
-                }
-              />
+              <Route index element={<RequireAuth><List /></RequireAuth>}/>   
+              <Route path=":productId" element={<RequireAuth><Product /></RequireAuth>}/>                  
+              <Route path="new" element={<RequireAuth><New inputs={products} title="Add new Product" /></RequireAuth>}/>  
             </Route>
           </Route>
         </Routes>

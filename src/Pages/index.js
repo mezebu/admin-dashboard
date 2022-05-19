cd50c46ad21f4838cfffaed6bbc15c477707f12c
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-19 13:54:20 +0100

    Update index.js

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -4,3 +4,4 @@ export { default as List } from "./List/List";
 export { default as Single } from "./Single/Single";
 export { default as New } from "./New/New";
 export { default as Analytics } from "./Analytics/Analytics";
+export { default as News } from "./News/News";

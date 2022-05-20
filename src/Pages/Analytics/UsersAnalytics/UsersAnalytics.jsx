2d35f3061d7e099976cacbcf564e09a74a451eff
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-20 08:33:23 +0100

    Create UsersAnalytics.jsx

---
+++ src/Pages/Analytics/UsersAnalytics/UsersAnalytics.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box, Toolbar } from "@mui/material";
+import AnalyticsTable from "./AnalyticsTable";
+
+const UsersAnalytics = () => {
+  return (
+    <Box>
+      <Toolbar />
+      <AnalyticsTable />
+    </Box>
+  );
+};
+
+export default UsersAnalytics;

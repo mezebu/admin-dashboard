fbbce47e7b4b6e9e08034982aa2dd2082787a6bd
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-21 08:01:21 +0100

    Update AnalyticsTable.jsx

--- src/Pages/Analytics/UsersAnalytics/AnalyticsTable.jsx
+++ src/Pages/Analytics/UsersAnalytics/AnalyticsTable.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+import { Box, Card, CardActions, CardContent } from "@mui/material";
+
+import { Button, Typography } from "@mui/material";
 
 const bull = (
   <Box

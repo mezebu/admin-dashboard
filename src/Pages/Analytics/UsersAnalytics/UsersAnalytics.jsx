646cad1554f70fa8990a62bcb84c0d70ac9f95fb
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-23 11:35:15 +0100

    Update UsersAnalytics.jsx

--- src/Pages/Analytics/UsersAnalytics/UsersAnalytics.jsx
+++ src/Pages/Analytics/UsersAnalytics/UsersAnalytics.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { Box, Toolbar } from "@mui/material";
+import { Box, Grid, Toolbar } from "@mui/material";
 import AnalyticsTable from "./AnalyticsTable";
 
 const UsersAnalytics = () => {
   return (
     <Box>
       <Toolbar />
-      <AnalyticsTable />
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={12} md={12} lg={8}>
+          <AnalyticsTable />
+        </Grid>
+      </Grid>
     </Box>
   );
 };

a29e4432a7a4dc2c9e94788f7579e2de768e6d27
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-23 11:35:11 +0100

    Create styles.js

---
+++ src/Pages/Analytics/UsersAnalytics/styles.js
@@ -0,0 +1,8 @@
+import { styled, TableCell, tableCellClasses } from "@mui/material";
+
+export const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+}));

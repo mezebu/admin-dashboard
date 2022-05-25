0f103b0703f0cd03900d762f58a5550df0ccb317
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-25 09:58:14 +0100

    Update AnalyticsTable.jsx

--- src/Pages/Analytics/UsersAnalytics/AnalyticsTable.jsx
+++ src/Pages/Analytics/UsersAnalytics/AnalyticsTable.jsx
@@ -1,23 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import * as dayjs from "dayjs";
+import { Avatar, Box, Button, Paper, Table, TableCell } from "@mui/material";
 import {
-  Avatar,
-  Box,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
+  Typography,
   TableRow,
+  TableHead,
+  TableContainer,
+  TableBody,
 } from "@mui/material";
-import { Typography } from "@mui/material";
+import SortIcon from "@mui/icons-material/Sort";
 import { StyledTableCell } from "./styles";
 import { transactions } from "./data";
 
 const AnalyticsTable = () => {
   const date = dayjs().format("D MMM YYYY");
   const time = dayjs().format("h:mm A");
+
+  const [transactionsArray, setTransactionsArray] = useState(transactions);
+  const [sortStatus, setSortStatus] = useState(true);
+
+  const handleAmountSort = () => {
+    let data = [...transactionsArray];
+    if (sortStatus) {
+      const sorted = data.sort((a, b) => a.amount - b.amount);
+      setTransactionsArray(sorted);
+      setSortStatus(!sortStatus);
+    } else {
+      const sorted = data.sort((a, b) => b.amount - a.amount);
+      setTransactionsArray(sorted);
+      setSortStatus(!sortStatus);
+    }
+  };
 
   return (
     <TableContainer component={Paper}>
@@ -32,60 +45,70 @@ const AnalyticsTable = () => {
           <TableRow>
             <StyledTableCell>Description</StyledTableCell>
             <StyledTableCell align="center">Date</StyledTableCell>
-            <StyledTableCell align="center">Amount</StyledTableCell>
+            <StyledTableCell align="center">
+              <Button
+                sx={{ textTransform: "none" }}
+                endIcon={<SortIcon />}
+                onClick={handleAmountSort}
+              >
+                Amount
+              </Button>
+            </StyledTableCell>
             <StyledTableCell align="center">Status</StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {transactions.map(({ id, image, label, user, amount, status }) => (
-            <TableRow
-              key={id}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                <Box sx={{ display: "flex", alignItems: "center" }}>
-                  <Avatar src={image} alt="user" />
-                  <Box sx={{ ml: 4 }}>
-                    <Typography variant="text.secondary">{label}</Typography>
-                    <Typography fontWeight={600}>{user}</Typography>
+          {transactionsArray.map(
+            ({ id, image, label, user, amount, status }) => (
+              <TableRow
+                key={id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  <Box sx={{ display: "flex", alignItems: "center" }}>
+                    <Avatar src={image} alt="user" />
+                    <Box sx={{ ml: 4 }}>
+                      <Typography variant="text.secondary">{label}</Typography>
+                      <Typography fontWeight={600}>{user}</Typography>
+                    </Box>
                   </Box>
-                </Box>
-              </TableCell>
-              <TableCell align="center">
-                <Typography variant="subtitle2">{date}</Typography>
-                <Typography variant="subtitle2"> {time}</Typography>
-              </TableCell>
-              <TableCell align="center">${amount}</TableCell>
-              <TableCell align="center">
-                <Typography
-                  sx={{
-                    fontSize: 13,
-                    fontWeight: 500,
-                    p: 1,
-                    background:
-                      status === "In progress"
-                        ? "rgba(255, 193, 7, 0.16)"
-                        : status === "Completed"
-                        ? "rgba(84, 214, 44, 0.16)"
-                        : status === "Failed"
-                        ? "rgba(255, 72, 66, 0.16)"
-                        : "rgb(255, 230, 255)",
-                    borderRadius: 1,
-                    color:
-                      status === "In progress"
-                        ? "rgb(183, 129, 3)"
-                        : status === "Completed"
-                        ? "rgb(34, 154, 22)"
-                        : status === "Failed"
-                        ? "rgb(183, 33, 54)"
-                        : "rgb(255, 77, 255)",
-                  }}
-                >
-                  {status}
-                </Typography>
-              </TableCell>
-            </TableRow>
-          ))}
+                </TableCell>
+                <TableCell align="center">
+                  <Typography variant="subtitle2">{date}</Typography>
+                  <Typography variant="subtitle2"> {time}</Typography>
+                </TableCell>
+                <TableCell align="center">${amount}</TableCell>
+                <TableCell align="center">
+                  <Typography
+                    sx={{
+                      fontSize: 13,
+                      fontWeight: 500,
+                      p: 1,
+                      background:
+                        status === "In progress"
+                          ? "rgba(255, 193, 7, 0.16)"
+                          : status === "Completed"
+                          ? "rgba(84, 214, 44, 0.16)"
+                          : status === "Failed"
+                          ? "rgba(255, 72, 66, 0.16)"
+                          : "rgb(255, 230, 255)",
+                      borderRadius: 1,
+                      color:
+                        status === "In progress"
+                          ? "rgb(183, 129, 3)"
+                          : status === "Completed"
+                          ? "rgb(34, 154, 22)"
+                          : status === "Failed"
+                          ? "rgb(183, 33, 54)"
+                          : "rgb(255, 77, 255)",
+                    }}
+                  >
+                    {status}
+                  </Typography>
+                </TableCell>
+              </TableRow>
+            )
+          )}
         </TableBody>
       </Table>
     </TableContainer>

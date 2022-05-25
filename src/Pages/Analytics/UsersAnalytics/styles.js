4ae82f8e5dacf10962bdb7eb96d7fda2c5aa88c4
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-25 09:58:21 +0100

    Update styles.js

--- src/Pages/Analytics/UsersAnalytics/styles.js
+++ src/Pages/Analytics/UsersAnalytics/styles.js
@@ -1,8 +1,25 @@
-import { styled, TableCell, tableCellClasses } from "@mui/material";
+import { styled, TableCell, tableCellClasses, TableRow } from "@mui/material";
 
 export const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
+    backgroundColor: theme.palette.error.light,
     color: theme.palette.common.white,
+  },
+}));
+
+export const StyledCountryTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.success.dark,
+    color: theme.palette.common.white,
+  },
+}));
+
+export const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
   },
 }));

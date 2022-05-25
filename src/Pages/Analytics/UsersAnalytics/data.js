692b8a7e5aa3560ef696d8f15a80434bf9f4fd73
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-25 09:58:18 +0100

    Update data.js

--- src/Pages/Analytics/UsersAnalytics/data.js
+++ src/Pages/Analytics/UsersAnalytics/data.js
@@ -42,9 +42,47 @@ export const transactions = [
     id: uuidv4(),
     label: "Payment for",
     user: "Insurance",
-    amount: "249",
+    amount: 249,
     status: "Completed",
     image:
       "https://images.unsplash.com/photo-1567186937675-a5131c8a89ea?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fHJhbmRvbSUyMHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500",
+  },
+];
+
+export const countries = [
+  {
+    id: uuidv4(),
+    country: "Spain",
+    flag: "https://flagcdn.com/w320/es.png",
+    seo: "35%",
+    value: "12,500",
+  },
+  {
+    id: uuidv4(),
+    country: "Nigeria",
+    flag: "https://flagcdn.com/w320/ng.png",
+    seo: "72%",
+    value: "500",
+  },
+  {
+    id: uuidv4(),
+    country: "Egypt",
+    flag: "https://flagcdn.com/w320/eg.png",
+    seo: "47%",
+    value: "500,00",
+  },
+  {
+    id: uuidv4(),
+    country: "Romania",
+    flag: "https://flagcdn.com/w320/ro.png",
+    seo: "95%",
+    value: "15,400",
+  },
+  {
+    id: uuidv4(),
+    country: "Norway",
+    flag: "https://flagcdn.com/w320/no.png",
+    seo: "100%",
+    value: "150,900",
   },
 ];

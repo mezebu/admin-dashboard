b76eb313a79b449c07c9f5bb621983a286524f2f
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-25 09:58:24 +0100

    Update UsersAnalytics.jsx

--- src/Pages/Analytics/UsersAnalytics/UsersAnalytics.jsx
+++ src/Pages/Analytics/UsersAnalytics/UsersAnalytics.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Box, Grid, Toolbar } from "@mui/material";
 import AnalyticsTable from "./AnalyticsTable";
+import AnalyticsCountries from "./AnalyticsCountries";
 
 const UsersAnalytics = () => {
   return (
     <Box>
       <Toolbar />
-      <Grid container spacing={3}>
+      <Grid container spacing={1}>
         <Grid item xs={12} sm={12} md={12} lg={8}>
           <AnalyticsTable />
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={4}>
+          <AnalyticsCountries />
         </Grid>
       </Grid>
     </Box>

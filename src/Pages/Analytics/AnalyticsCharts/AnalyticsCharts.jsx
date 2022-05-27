408e200346e4451f7ae3341d8479ceb2896c30fd
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-05-27 08:21:40 +0100

    Update AnalyticsCharts.jsx

--- src/Pages/Analytics/AnalyticsCharts/AnalyticsCharts.jsx
+++ src/Pages/Analytics/AnalyticsCharts/AnalyticsCharts.jsx
@@ -1,14 +1,21 @@
-import { Grid } from "@mui/material";
+import { Box, Grid, Toolbar } from "@mui/material";
 import React from "react";
+import AnalyticsLine from "./AnalyticsLine";
 import AnalyticsPie from "./AnalyticsPie";
 
 const AnalyticsCharts = () => {
   return (
-    <Grid container spacing={1}>
-      <Grid item xs={12} sm={12} md={12} lg={8}>
-        <AnalyticsPie />
+    <Box>
+      <Toolbar />
+      <Grid container spacing={1}>
+        <Grid item xs={12} sm={12} md={12} lg={8}>
+          <AnalyticsLine />
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={4}>
+          <AnalyticsPie />
+        </Grid>
       </Grid>
-    </Grid>
+    </Box>
   );
 };
 

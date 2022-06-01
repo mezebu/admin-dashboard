b3dc0a6a77514fe0f11b6cdc38dd2c4ddc2697bf
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-06-01 09:43:05 +0100

    Update News.jsx

--- src/Pages/News/News.jsx
+++ src/Pages/News/News.jsx
@@ -1,28 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Box, CssBaseline, Toolbar } from "@mui/material";
+
 import Appbar from "../../components/Appbar/Appbar";
 import SideNav from "../../components/SideNav/SideNav";
-
-import { fetchData } from "../../api";
+import NewsData from "./NewsData";
 
 const drawerWidth = 240;
 
 const News = () => {
   const [mobileOpen, setMobileOpen] = useState(false);
-  const [newsData, setNewsData] = useState([]);
-
-  console.log(newsData);
 
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
-
-  useEffect(() => {
-    const getNews = async () => {
-      setNewsData(fetchData());
-    };
-    getNews();
-  }, []);
 
   return (
     <Box sx={{ display: { sm: "flex" } }}>
@@ -42,7 +32,9 @@ const News = () => {
         }}
       >
         <Toolbar />
-        <Box component="div">news</Box>
+        <Box component="div">
+          <NewsData />
+        </Box>
       </Box>
     </Box>
   );

361220f996fd5e40925705fd0bea5b99c4709b27
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-06-02 09:03:13 +0100

    Create styles.js

---
+++ src/Pages/News/styles.js
@@ -0,0 +1,8 @@
+import { Box, styled } from "@mui/material";
+
+export const Wrapper = styled(Box)({
+  height: "80vh",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});

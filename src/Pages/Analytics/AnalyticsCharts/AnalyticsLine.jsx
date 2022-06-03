b64d0575c109de127e242790fbfea241c0f53cc8
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-06-03 11:05:17 +0100

    Update AnalyticsLine.jsx

--- src/Pages/Analytics/AnalyticsCharts/AnalyticsLine.jsx
+++ src/Pages/Analytics/AnalyticsCharts/AnalyticsLine.jsx
@@ -1,8 +1,45 @@
 import React from "react";
-import { Paper } from "@mui/material";
+import { Card, CardContent, CardHeader, Divider } from "@mui/material";
+import { IconButton, Tooltip as MuiTooltip, Typography } from "@mui/material";
+import InfoIcon from "@mui/icons-material/Info";
+import { Line } from "react-chartjs-2";
+import { data, options } from "./chartData";
+import { Chart, CategoryScale, LinearScale, PointElement } from "chart.js";
+import { LineElement, Title, Tooltip, Legend } from "chart.js";
+
+Chart.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const AnalyticsLine = () => {
-  return <Paper>Analytics Line</Paper>;
+  return (
+    <Card>
+      <CardHeader
+        action={
+          <IconButton>
+            <MuiTooltip title="Traffic Sources">
+              <InfoIcon />
+            </MuiTooltip>
+          </IconButton>
+        }
+        title={
+          <Typography variant="h6" sx={{ fontWeight: 600 }}>
+            Traffic Sources
+          </Typography>
+        }
+      />
+      <Divider />
+      <CardContent>
+        <Line data={data} options={options} />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default AnalyticsLine;

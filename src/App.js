55369e85c1a491d896d0bf54f340dacba5bfd993
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-06-07 10:10:05 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { useContext } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 
-import { Home, List, Login, New, Single, Analytics, News } from "./Pages";
+import { Home, List, Login, New, Single, Analytics, News, Products } from "./Pages";
 import { formInputs, products } from "./formInputs";
 import { useThemeContext } from "./contexts/ThemeContext";
 import { darkMode, lightMode } from "./styles";
 import Product from "./Pages/Product/Product";
 import { AuthContext } from "./contexts/AuthContext";
+
 
 
 function App() {
@@ -34,7 +35,7 @@ function App() {
               <Route path="analytics" element={<RequireAuth><Analytics /></RequireAuth>}/>               
             </Route>
             <Route path="products">
-              <Route index element={<RequireAuth><List /></RequireAuth>}/>   
+              <Route index element={<RequireAuth><Products /></RequireAuth>}/>   
               <Route path=":productId" element={<RequireAuth><Product /></RequireAuth>}/>                  
               <Route path="new" element={<RequireAuth><New inputs={products} title="Add new Product" /></RequireAuth>}/>  
             </Route>

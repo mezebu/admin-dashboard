ba53fb9d1ba850e7fc66bcfd7c49f9129a093b89
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-06-08 10:25:54 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, { useContext } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 
-import { Home, List, Login, New, Single, Analytics, News, Products } from "./Pages";
+import { Home, List, Login, New, Single, Analytics, News, Products, Logistics, Customers } from "./Pages";
 import { formInputs, products } from "./formInputs";
 import { useThemeContext } from "./contexts/ThemeContext";
 import { darkMode, lightMode } from "./styles";
-import Product from "./Pages/Product/Product";
 import { AuthContext } from "./contexts/AuthContext";
 
 
@@ -27,7 +26,9 @@ function App() {
           <Route path="/">
             <Route path="login" element={<Login />} />
             <Route index element={<RequireAuth><Home /></RequireAuth>} />        
-            <Route path="news" element={<RequireAuth><News /></RequireAuth>} />        
+            <Route path="news" element={<RequireAuth><News /></RequireAuth>} />    
+             <Route path="logistics" element={<RequireAuth><Logistics /></RequireAuth>}/>      
+             <Route path="customers" element={<RequireAuth><Customers /></RequireAuth>}/>      
             <Route path="users">
               <Route index element={<RequireAuth><List /></RequireAuth>}/>          
               <Route path=":userId" element={<RequireAuth><Single /></RequireAuth>}/>     
@@ -35,8 +36,7 @@ function App() {
               <Route path="analytics" element={<RequireAuth><Analytics /></RequireAuth>}/>               
             </Route>
             <Route path="products">
-              <Route index element={<RequireAuth><Products /></RequireAuth>}/>   
-              <Route path=":productId" element={<RequireAuth><Product /></RequireAuth>}/>                  
+              <Route index element={<RequireAuth><Products /></RequireAuth>}/>                   
               <Route path="new" element={<RequireAuth><New inputs={products} title="Add new Product" /></RequireAuth>}/>  
             </Route>
           </Route>

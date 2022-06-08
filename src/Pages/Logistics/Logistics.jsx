43b980c3f53e49d53e0854202325a9efd5e99035
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-06-08 10:26:14 +0100

    Create Logistics.jsx

---
+++ src/Pages/Logistics/Logistics.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Box, CssBaseline, Toolbar } from "@mui/material";
+import Appbar from "../../components/Appbar/Appbar";
+import SideNav from "../../components/SideNav/SideNav";
+
+const drawerWidth = 240;
+
+const Logistics = () => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  return (
+    <Box sx={{ display: { sm: "flex" } }}>
+      <CssBaseline />
+      <Appbar drawerToggle={handleDrawerToggle} drawerWidth={drawerWidth} />
+      <SideNav
+        drawerWidth={drawerWidth}
+        mobileOpen={mobileOpen}
+        drawerToggle={handleDrawerToggle}
+      />
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        <Toolbar />
+        <Box>Logistics</Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Logistics;

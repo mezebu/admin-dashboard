4e5ec444cce2cc89cb9f7c666e7a82b283b20b53
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-06-10 00:52:57 +0100

    Update Customers.jsx

--- src/Pages/Customers/Customers.jsx
+++ src/Pages/Customers/Customers.jsx
@@ -1,42 +1,15 @@
-import React, { useState } from "react";
-import { Box, CssBaseline, Toolbar } from "@mui/material";
-import Appbar from "../../components/Appbar/Appbar";
-import SideNav from "../../components/SideNav/SideNav";
+import React from "react";
+
 import CustomersTable from "./CustomersTable";
 import CustomersHeader from "./CustomersHeader";
-
-const drawerWidth = 240;
+import Drawer from "../../components/Drawer/Drawer";
 
 const Customers = () => {
-  const [mobileOpen, setMobileOpen] = useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
   return (
-    <Box sx={{ display: { sm: "flex" } }}>
-      <CssBaseline />
-      <Appbar drawerToggle={handleDrawerToggle} drawerWidth={drawerWidth} />
-      <SideNav
-        drawerWidth={drawerWidth}
-        mobileOpen={mobileOpen}
-        drawerToggle={handleDrawerToggle}
-      />
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-        }}
-      >
-        <Toolbar />
-        <Box>
-          <CustomersHeader />
-          <CustomersTable />
-        </Box>
-      </Box>
-    </Box>
+    <Drawer>
+      <CustomersHeader />
+      <CustomersTable />
+    </Drawer>
   );
 };
 

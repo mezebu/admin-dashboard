80c8efcc7f69af5f511484b8dc834143175f4ee2
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-06-10 00:52:51 +0100

    Update Analytics.jsx

--- src/Pages/Analytics/Analytics.jsx
+++ src/Pages/Analytics/Analytics.jsx
@@ -1,50 +1,25 @@
-import React, { useState } from "react";
-import { Box, CssBaseline, Toolbar, Typography } from "@mui/material";
-import Appbar from "../../components/Appbar/Appbar";
-import SideNav from "../../components/SideNav/SideNav";
+import React from "react";
+import { Box, Toolbar, Typography } from "@mui/material";
+
 import AnalyticsGrid from "./AnalyticsWidget/AnalyticsGrid";
 import UsersAnalytics from "./UsersAnalytics/UsersAnalytics";
 import AnalyticsCharts from "./AnalyticsCharts/AnalyticsCharts";
-
-const drawerWidth = 240;
+import Drawer from "../../components/Drawer/Drawer";
 
 const Analytics = () => {
-  const [mobileOpen, setMobileOpen] = useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
   return (
-    <Box sx={{ display: { sm: "flex" } }}>
-      <CssBaseline />
-      <Appbar drawerToggle={handleDrawerToggle} drawerWidth={drawerWidth} />
-      <SideNav
-        drawerWidth={drawerWidth}
-        mobileOpen={mobileOpen}
-        drawerToggle={handleDrawerToggle}
-      />
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-        }}
-      >
-        <Toolbar />
-        <Box component="div">
-          <Typography variant="h5" fontWeight={700} gutterBottom>
-            Hi, Welcome back!
-          </Typography>
+    <Drawer>
+      <Box component="div">
+        <Typography variant="h5" fontWeight={700} gutterBottom>
+          Hi, Welcome back!
+        </Typography>
 
-          <Toolbar />
-          <AnalyticsGrid />
-          <AnalyticsCharts />
-          <UsersAnalytics />
-        </Box>
+        <Toolbar />
+        <AnalyticsGrid />
+        <AnalyticsCharts />
+        <UsersAnalytics />
       </Box>
-    </Box>
+    </Drawer>
   );
 };
 

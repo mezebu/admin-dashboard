0348cfc16cbcfae7ba6d9eb5c7f62ab615d9ece9
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-06-12 03:43:20 +0100

    Update Products.jsx

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,41 +1,13 @@
-import React, { useState } from "react";
-import { Box, CssBaseline, Toolbar } from "@mui/material";
-import Appbar from "../../components/Appbar/Appbar";
-import SideNav from "../../components/SideNav/SideNav";
+import React from "react";
+
+import Drawer from "../../components/Drawer/Drawer";
 import ProductData from "./ProductData";
 
-const drawerWidth = 240;
-
 const Products = () => {
-  const [mobileOpen, setMobileOpen] = useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
   return (
-    <Box sx={{ display: { sm: "flex" } }}>
-      <CssBaseline />
-      <Appbar drawerToggle={handleDrawerToggle} drawerWidth={drawerWidth} />
-      <SideNav
-        drawerWidth={drawerWidth}
-        mobileOpen={mobileOpen}
-        drawerToggle={handleDrawerToggle}
-      />
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-        }}
-      >
-        <Toolbar />
-        <Box>
-          <ProductData />
-        </Box>
-      </Box>
-    </Box>
+    <Drawer>
+      <ProductData />
+    </Drawer>
   );
 };
 

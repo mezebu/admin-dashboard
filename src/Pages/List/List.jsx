b904da528ddfded49dd74e4bb95dcebf64c93301
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-06-12 03:43:25 +0100

    Update List.jsx

--- src/Pages/List/List.jsx
+++ src/Pages/List/List.jsx
@@ -1,82 +1,46 @@
-import React, { useState } from "react";
-import {
-  Box,
-  Button,
-  Divider,
-  Grid,
-  Typography,
-  CssBaseline,
-  Toolbar,
-} from "@mui/material";
+import { Box, Button, Divider, Grid, Typography } from "@mui/material";
 import UsersTable from "../../components/UsersTable/UsersTable";
 import { useNavigate } from "react-router-dom";
-
-import Appbar from "../../components/Appbar/Appbar";
-import SideNav from "../../components/SideNav/SideNav";
-
-const drawerWidth = 240;
+import Drawer from "../../components/Drawer/Drawer";
 
 const List = () => {
   const navigate = useNavigate();
-  const [mobileOpen, setMobileOpen] = useState(false);
 
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
   return (
-    <Box sx={{ display: { sm: "flex" } }}>
-      <CssBaseline />
-      <Appbar drawerToggle={handleDrawerToggle} drawerWidth={drawerWidth} />
-      <SideNav
-        drawerWidth={drawerWidth}
-        mobileOpen={mobileOpen}
-        drawerToggle={handleDrawerToggle}
-      />
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-        }}
-      >
-        <Toolbar />
-        <Box component="div">
-          <Grid container sx={{ mt: 3 }}>
-            <Grid item xs={12} sm={6} md={6} lg={6}>
-              <Box>
-                <Typography variant="h4" fontWeight={700} gutterBottom>
-                  Users
-                </Typography>
-              </Box>
-            </Grid>
-            <Grid item xs={12} sm={6} md={6} lg={6}>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: {
-                    lg: "flex-end",
-                    md: "flex-end",
-                    sm: "flex-end",
-                  },
-                }}
-              >
-                <Button
-                  variant="contained"
-                  disableElevation
-                  sx={{ textTransform: "none" }}
-                  onClick={() => navigate("/users/new")}
-                >
-                  Add New User
-                </Button>
-              </Box>
-            </Grid>
-          </Grid>
-          <Divider sx={{ mt: 2, mb: 5 }} />
-          <UsersTable />
-        </Box>
-      </Box>
-    </Box>
+    <Drawer>
+      <Grid container sx={{ mt: 3 }}>
+        <Grid item xs={12} sm={6} md={6} lg={6}>
+          <Box>
+            <Typography variant="h4" fontWeight={700} gutterBottom>
+              Users
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6} md={6} lg={6}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: {
+                lg: "flex-end",
+                md: "flex-end",
+                sm: "flex-end",
+              },
+            }}
+          >
+            <Button
+              variant="contained"
+              disableElevation
+              sx={{ textTransform: "none" }}
+              onClick={() => navigate("/users/new")}
+            >
+              Add New User
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+      <Divider sx={{ mt: 2, mb: 5 }} />
+      <UsersTable />
+    </Drawer>
   );
 };
 

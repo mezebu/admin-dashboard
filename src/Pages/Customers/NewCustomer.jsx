5e00c501895dfc83eb481d0e322cd2d0fa1bb205
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-06-15 23:17:35 +0100

    Create NewCustomer.jsx

---
+++ src/Pages/Customers/NewCustomer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Drawer from "../../components/Drawer/Drawer";
+
+const NewCustomer = () => {
+  return <Drawer>NewCustomer</Drawer>;
+};
+
+export default NewCustomer;

af9e889f30b7e4a78329e769f9e3727e19d1f387
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-06-16 00:41:40 +0100

    Update Home.jsx

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,43 +1,13 @@
-import { useState } from "react";
-import { Box, CssBaseline, Toolbar } from "@mui/material";
-
 import Widgets from "../../components/Widgets/Widgets";
 import Charts from "../../components/Charts/Charts";
-import Appbar from "../../components/Appbar/Appbar";
-import SideNav from "../../components/SideNav/SideNav";
-
-const drawerWidth = 240;
+import Drawer from "../../components/Drawer/Drawer";
 
 const Home = () => {
-  const [mobileOpen, setMobileOpen] = useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
   return (
-    <Box sx={{ display: { sm: "flex" } }}>
-      <CssBaseline />
-      <Appbar drawerToggle={handleDrawerToggle} drawerWidth={drawerWidth} />
-      <SideNav
-        drawerWidth={drawerWidth}
-        mobileOpen={mobileOpen}
-        drawerToggle={handleDrawerToggle}
-      />
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-        }}
-      >
-        <Toolbar />
-        <Box component="div">
-          <Widgets />
-          <Charts />
-        </Box>
-      </Box>
-    </Box>
+    <Drawer>
+      <Widgets />
+      <Charts />
+    </Drawer>
   );
 };
 

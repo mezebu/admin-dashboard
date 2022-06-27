ba592481821ba1b4e9a0a7638945e009887f3a51
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-06-27 10:52:49 +0100

    Update AnalyticsPie.jsx

--- src/Pages/Analytics/AnalyticsCharts/AnalyticsPie.jsx
+++ src/Pages/Analytics/AnalyticsCharts/AnalyticsPie.jsx
@@ -1,52 +1,14 @@
 import React from "react";
 import { Box, Divider, Tooltip as MuiTooltip, Typography } from "@mui/material";
-import {
-  Button,
-  Card,
-  CardHeader,
-  IconButton,
-  CardContent,
-} from "@mui/material";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
+import { Button, Card, CardHeader } from "@mui/material";
+import { IconButton, CardContent } from "@mui/material";
 import InfoIcon from "@mui/icons-material/Info";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+import { pieData, pieLabelPosition } from "./chartData";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
-
-const data = {
-  labels: ["Instagram", "LinkedIn", "Twitter", "Other"],
-  datasets: [
-    {
-      label: "# of Votes",
-      data: [12, 19, 65, 9],
-      backgroundColor: [
-        "rgba(63, 120, 43, 1)",
-        "rgba(14, 118, 168, 1)",
-        "rgba(0, 172, 238, 1)",
-        "rgba(159, 72, 43, 1)",
-      ],
-      borderColor: [
-        "rgba(255, 99, 132, 1)",
-        "rgba(54, 162, 235, 1)",
-        "rgba(255, 206, 86, 1)",
-        "rgba(255, 150, 173, 1)",
-      ],
-      borderWidth: 1,
-      spacing: 4,
-      cutout: "70%",
-      borderRadius: 3,
-    },
-  ],
-};
-
-const labelPosition = {
-  plugins: {
-    legend: {
-      position: "bottom",
-    },
-  },
-};
 
 const AnalyticsPie = () => {
   return (
@@ -67,7 +29,7 @@ const AnalyticsPie = () => {
       />
       <Divider />
       <CardContent sx={{ p: 5 }}>
-        <Doughnut data={data} options={labelPosition} />
+        <Doughnut data={pieData} options={pieLabelPosition} />
       </CardContent>
       <Divider />
 

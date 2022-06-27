526334eaf786202ba676ed437d5efd10f1e6cb0f
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-06-27 10:52:55 +0100

    Update NewCustomer.jsx

--- src/Pages/Customers/NewCustomer.jsx
+++ src/Pages/Customers/NewCustomer.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box, Grid, Paper, TextField, Toolbar } from "@mui/material";
+
 import Drawer from "../../components/Drawer/Drawer";
 
 const NewCustomer = () => {
-  return <Drawer>NewCustomer</Drawer>;
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+
+  return (
+    <Drawer>
+      <Toolbar />
+      <Paper sx={{ p: 4 }}>
+        <Box component="form">
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={5}>
+              <TextField
+                required
+                fullWidth
+                id="first-name"
+                label="First Name"
+                value={firstname}
+                onChange={(e) => setFirstname(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={5}>
+              <TextField
+                required
+                fullWidth
+                id="last-name"
+                label="Last Name"
+                value={lastname}
+                onChange={(e) => setLastname(e.target.value)}
+              />
+            </Grid>
+          </Grid>
+        </Box>
+      </Paper>
+    </Drawer>
+  );
 };
 
 export default NewCustomer;

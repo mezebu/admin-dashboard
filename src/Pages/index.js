49d9a364d615b1ed2d263c321fafb4064b159355
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-06-29 09:47:31 +0100

    Update index.js

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,7 +1,6 @@
 export { default as Home } from "./Home/Home";
 export { default as Login } from "./Login/Login";
 export { default as List } from "./List/List";
-export { default as Single } from "./Single/Single";
 export { default as New } from "./New/New";
 export { default as Analytics } from "./Analytics/Analytics";
 export { default as News } from "./News/News";
@@ -9,4 +8,4 @@ export { default as Products } from "./Products/Products";
 export { default as Logistics } from "./Logistics/Logistics";
 export { default as Customers } from "./Customers/Customers";
 export { default as NewCustomer } from "./Customers/NewCustomer";
-export { default as EditCustomer } from "./Customers/EditCustomer";
+export { default as EditCustomer } from "./Customers/Edit/EditCustomer";

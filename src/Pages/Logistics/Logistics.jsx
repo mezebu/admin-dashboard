8fb5518cba23c02b91fb0e1ecfb027f6f8460d5e
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-05 09:17:03 +0100

    Update Logistics.jsx

--- src/Pages/Logistics/Logistics.jsx
+++ src/Pages/Logistics/Logistics.jsx
@@ -1,38 +1,17 @@
-import React, { useState } from "react";
-import { Box, CssBaseline, Toolbar } from "@mui/material";
-import Appbar from "../../components/Appbar/Appbar";
-import SideNav from "../../components/SideNav/SideNav";
+import React from "react";
+import { Typography } from "@mui/material";
 
-const drawerWidth = 240;
+import Drawer from "../../components/Drawer/Drawer";
+import LogisticsWidgets from "./LogisticsWidgets";
 
 const Logistics = () => {
-  const [mobileOpen, setMobileOpen] = useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
   return (
-    <Box sx={{ display: { sm: "flex" } }}>
-      <CssBaseline />
-      <Appbar drawerToggle={handleDrawerToggle} drawerWidth={drawerWidth} />
-      <SideNav
-        drawerWidth={drawerWidth}
-        mobileOpen={mobileOpen}
-        drawerToggle={handleDrawerToggle}
-      />
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-        }}
-      >
-        <Toolbar />
-        <Box>Logistics</Box>
-      </Box>
-    </Box>
+    <Drawer>
+      <Typography variant="h6" fontWeight={600}>
+        Logistics
+      </Typography>
+      <LogisticsWidgets />
+    </Drawer>
   );
 };
 

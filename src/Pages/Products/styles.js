cc3a6ccf2aff9277f459fb1d7a4fb165fe213dbf
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-05 09:17:10 +0100

    Create styles.js

---
+++ src/Pages/Products/styles.js
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material";
+
+const darkMode = createTheme({
+  palette: {
+    mode: "dark",
+  },
+  typography: {
+    fontFamily: ["Quicksand"].join(","),
+  },
+});
+
+const lightMode = createTheme({
+  palette: {
+    mode: "light",
+  },
+  typography: {
+    fontFamily: ["Quicksand"].join(","),
+  },
+});
+export { theme, darkMode, lightMode };

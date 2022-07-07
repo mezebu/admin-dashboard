7125d7ad05fd712208562af7bfd082d53fa2546d
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-07 10:37:41 +0100

    Create LogisticsCondition.jsx

---
+++ src/Pages/Logistics/LogisticsCondition.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  Box,
+  Card,
+  Divider,
+  Grid,
+  Paper,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { vehiclesConditions } from "./data";
+
+const LogisticsCondition = () => {
+  return (
+    <>
+      <Toolbar />
+      <Paper variant="outlined">
+        <Box sx={{ p: 3 }}>
+          <Typography variant="h6" sx={{ fontWeight: 600 }}>
+            Vehicle Conditions
+          </Typography>
+        </Box>
+        <Divider />
+        <Box sx={{ p: 3 }}>
+          <Grid container spacing={2}>
+            {vehiclesConditions.map(({ id, title, chart, value, subTitle }) => (
+              <Grid key={id} item lg={4} md={12} sm={12} xs={12}>
+                <Card variant="outlined" sx={{ minWidth: 200 }}>
+                  <Typography
+                    sx={{ mt: 1, fontSize: 17, fontWeight: 600 }}
+                    align="center"
+                  >
+                    {title}
+                  </Typography>
+                  <Box>{chart}</Box>
+                  <Typography
+                    variant="body1"
+                    sx={{ fontWeight: 600 }}
+                    align="center"
+                  >
+                    {value}
+                  </Typography>
+                  <Typography variant="subtitle2" align="center" sx={{ mb: 2 }}>
+                    {subTitle}
+                  </Typography>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </Paper>
+    </>
+  );
+};
+
+export default LogisticsCondition;

57ec1f98d5f4f47684c9c654d09d47d30f584987
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-07-08 11:05:51 +0100

    Update AnalyticsGrid.jsx

--- src/Pages/Analytics/AnalyticsWidget/AnalyticsGrid.jsx
+++ src/Pages/Analytics/AnalyticsWidget/AnalyticsGrid.jsx
@@ -1,26 +1,23 @@
 import { Grid } from "@mui/material";
 import React from "react";
-import AnimatedPage from "../../../components/PageAnimatons/AnimatedPage";
 import AnalyticsWidget from "./AnalyticsWidget";
 
 const AnalyticsGrid = () => {
   return (
-    <AnimatedPage>
-      <Grid container spacing={3}>
-        <Grid item xs={12} sm={6} md={6} lg={3}>
-          <AnalyticsWidget type="android" />
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={3}>
-          <AnalyticsWidget type="apple" />
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={3}>
-          <AnalyticsWidget type="reports" />
-        </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={3}>
-          <AnalyticsWidget type="orders" />
-        </Grid>
+    <Grid container spacing={3}>
+      <Grid item xs={12} sm={6} md={6} lg={3}>
+        <AnalyticsWidget type="android" />
       </Grid>
-    </AnimatedPage>
+      <Grid item xs={12} sm={6} md={6} lg={3}>
+        <AnalyticsWidget type="apple" />
+      </Grid>
+      <Grid item xs={12} sm={6} md={6} lg={3}>
+        <AnalyticsWidget type="reports" />
+      </Grid>
+      <Grid item xs={12} sm={6} md={6} lg={3}>
+        <AnalyticsWidget type="orders" />
+      </Grid>
+    </Grid>
   );
 };
 

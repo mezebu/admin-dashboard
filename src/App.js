b399758dd6c3f1f86ef259f38b434f752651c6c6
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-08-10 10:57:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React, { useContext, useState, useEffect } from "react";
-import {  Routes, Route, Navigate, useLocation } from "react-router-dom";
+import React, {  useState, useEffect } from "react";
+import {  Routes, Route,useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import { ThemeProvider } from "@mui/material/styles";
 import {ToastContainer} from 'react-toastify'
 import { Triangle } from "react-loader-spinner";
 
 import "react-toastify/dist/ReactToastify.css";
-import { Home, List, Login, New, Analytics, News, Products, Logistics, Customers, NewCustomer, EditCustomer, TodoList } from "./Pages";
+import { Home, List, New, Analytics, News, Products, Logistics, Customers, NewCustomer, EditCustomer, TodoList } from "./Pages";
 import { formInputs, products } from "./formInputs";
 import { useThemeContext } from "./contexts/ThemeContext";
 import { darkMode, lightMode, Loader } from "./styles";
-import { AuthContext } from "./contexts/AuthContext";
 
 function App() {
   const location = useLocation()
   const darkTheme = useThemeContext();
   const switchTheme = darkTheme ? darkMode : lightMode;
-  const { currentUser } = useContext(AuthContext);
 
   const [loader, setLoader] = useState(true)
 
@@ -26,10 +24,6 @@ function App() {
     }, 5000);
   }, [])
 
-  const RequireAuth = ({ children }) => {
-    return currentUser ? children : <Navigate to="/login" />;
-  };
-
   return (
     <ThemeProvider theme={switchTheme}>
       {loader ? (<Loader ><Triangle height="150" width="150" ariaLabel="loading-indicator" /></Loader>) : (
@@ -37,24 +31,23 @@ function App() {
         <ToastContainer autoClose={2000} />
         <Routes key={location.pathname} location={location}>
           <Route path="/">
-            <Route path="login" element={<Login />} />
-            <Route index element={<RequireAuth><Home /></RequireAuth>} />        
-            <Route path="news" element={<RequireAuth><News /></RequireAuth>} />    
-             <Route path="logistics" element={<RequireAuth><Logistics /></RequireAuth>}/>            
+            <Route index element={<Home />} />        
+            <Route path="news" element={<News />} />    
+             <Route path="logistics" element={<Logistics />}/>            
             <Route path="users">
-              <Route index element={<RequireAuth><List /></RequireAuth>}/>             
-              <Route path="new" element={<RequireAuth><New inputs={formInputs} title="Add new user" /></RequireAuth>}/>               
-              <Route path="analytics" element={<RequireAuth><Analytics /></RequireAuth>}/>               
-              <Route path="todolist" element={<RequireAuth><TodoList /></RequireAuth>}/>               
+              <Route index element={<List />}/>             
+              <Route path="new" element={<New inputs={formInputs} title="Add new user" />}/>               
+              <Route path="analytics" element={<Analytics />}/>               
+              <Route path="todolist" element={<TodoList />}/>               
             </Route>
             <Route path="products">
-              <Route index element={<RequireAuth><Products /></RequireAuth>}/>                   
-              <Route path="new" element={<RequireAuth><New inputs={products} title="Add new Product" /></RequireAuth>}/>  
+              <Route index element={<Products />}/>                   
+              <Route path="new" element={<New inputs={products} title="Add new Product" />}/>  
             </Route>
             <Route path="customers">
-            <Route index element={<RequireAuth><Customers /></RequireAuth>}/> 
-            <Route path=":id" element={<RequireAuth><EditCustomer /></RequireAuth>}/>
-            <Route path='newcustomer' element={<RequireAuth><NewCustomer /></RequireAuth>}/>
+            <Route index element={<Customers />}/> 
+            <Route path=":id" element={<EditCustomer />}/>
+            <Route path='newcustomer' element={<NewCustomer />}/>
             </Route>
           </Route>
         </Routes>

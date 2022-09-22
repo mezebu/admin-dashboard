4fb0ef3a6221117ab8eb3edf43ab639a81757d84
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-22 09:53:48 +0100

    Update styles.js

--- src/components/Appbar/styles.js
+++ src/components/Appbar/styles.js
@@ -1,49 +1,12 @@
-import { alpha, Badge, InputBase, useScrollTrigger } from "@mui/material";
+import { AppBar, Badge, useScrollTrigger } from "@mui/material";
 import { createTheme, styled } from "@mui/material/styles";
 import { cloneElement } from "react";
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  marginRight: theme.spacing(1),
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "12ch",
-      "&:focus": {
-        width: "20ch",
-      },
-    },
-  },
-}));
+export const StyledAppBar = styled(AppBar)({
+  background: "rgba( 255, 255, 255, 0 )",
+  border: "1px solid rgba( 255, 255, 255, 0.18 )",
+  backdropFilter: "blur(17px)",
+});
 
 const StyledBadge = styled(Badge)(({ theme }) => ({
   "& .MuiBadge-badge": {
@@ -104,12 +67,4 @@ const lightMode = createTheme({
   },
 });
 
-export {
-  Search,
-  ElevationScroll,
-  SearchIconWrapper,
-  StyledInputBase,
-  StyledBadge,
-  darkMode,
-  lightMode,
-};
+export { ElevationScroll, StyledBadge, darkMode, lightMode };

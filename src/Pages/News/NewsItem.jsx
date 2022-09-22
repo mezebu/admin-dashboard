7732e8e9cea152a10b50c7b522a06546da341f49
Ezeigwe Henry <65811270+Mezebu@users.noreply.github.com>
2022-09-22 09:54:06 +0100

    Update NewsItem.jsx

--- src/Pages/News/NewsItem.jsx
+++ src/Pages/News/NewsItem.jsx
@@ -1,38 +1,39 @@
 import React from "react";
-import { Button, CardActions, Grid, Typography } from "@mui/material";
 import { Card, CardContent, CardMedia } from "@mui/material";
-
-import { green } from "@mui/material/colors";
+import { Button, Grid, Typography } from "@mui/material";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
+import { StyledTitle } from "./styles";
 
 const NewsItem = ({ urlToImage, source, title, url }) => {
   return (
     <Grid item lg={4} md={6} sm={12} xs={12}>
-      <Card elevation={0} sx={{ height: 320 }}>
+      <Card elevation={0} sx={{ height: 350 }}>
         <CardMedia
           component="img"
-          height="160"
+          height="50%"
           image={urlToImage}
           alt="image source"
         />
         <CardContent>
-          <Typography
-            gutterBottom
-            variant="subtitle2"
-            component="span"
-            sx={{ background: green[700], p: 0.5, borderRadius: 1 }}
-          >
-            {source.name}
-          </Typography>
+          <StyledTitle>
+            <Typography variant="button" sx={{ fontWeight: 900 }}>
+              {source.name}
+            </Typography>
+            <Button
+              sx={{ textTransform: "none" }}
+              href={url}
+              target="_blank"
+              endIcon={<ArrowForwardIcon />}
+            >
+              Full story
+            </Button>
+          </StyledTitle>
+
           <Typography variant="subtitle1" fontWeight={500}>
             {title}
           </Typography>
         </CardContent>
-        <CardActions>
-          <Button href={url} target="_blank" endIcon={<ArrowForwardIcon />}>
-            Full story
-          </Button>
-        </CardActions>
       </Card>
     </Grid>
   );
